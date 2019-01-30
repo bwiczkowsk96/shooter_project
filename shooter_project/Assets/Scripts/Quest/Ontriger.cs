418628f0ef78a22da1053935681d44e74d9c8ed2
Thandorr <jasiurkowski.kamil@gmail.com>
2019-01-30 20:33:03 +0100

    Pliki naprawione

---
+++ shooter_project/Assets/Scripts/Quest/Ontriger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ontriger : MonoBehaviour {
+
+   
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("wlazl");
+    }
+}
