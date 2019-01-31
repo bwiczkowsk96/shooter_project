783902397d308a39df60fc3eea31c2de2d6a32c1
vicebal <33986657+vicebal@users.noreply.github.com>
2019-01-31 19:46:21 +0100

    shoot

---
+++ shooter_project/Assets/AKM/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    public float health;
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+            Die();
+
+		
+	}
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
